6acec05d56d5eeb90bcf772f5e47f4ad042e0a31
nuharaomesh <omeshnuhara4@gmail.com>
2025-02-28 18:03:18 +0530

    impl Application provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
+import { ApplicationProvider } from "@ui-kitten/components";
 import {Stack} from "expo-router";
-import { StyleSheet } from "react-native";
+import * as eva from '@eva-design/eva';
 
 export default function Layout() {
     return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-        </Stack>
+        <ApplicationProvider {...eva} theme={eva.dark}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
+            </Stack>
+        </ApplicationProvider>   
     )
 }
